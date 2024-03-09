21e33bf3a48335376dcdcc86a18c29869e630b52
Balivern <123549102+Balivern@users.noreply.github.com>
2024-03-09 15:33:59 +0100

    Initial commit

---
+++ Tests/factr.ml
@@ -0,0 +1,18 @@
+(* Tail-recursive and non tail-recursive verions of factorial *)
+
+let rec factr (n : int) (acc: int): int = 
+  if n = 0
+  then acc
+  else factr (n - 1) (n * acc)
+;;
+
+let rec fac (n : int) : int =
+  if n = 0 then 1 else n * fac (n - 1)
+;;
+
+let rec foo (n : int) : int = if 2 then 5 else 6 ;;
+
+
+(* the two functions produce the same result for argument 10 *)
+factr 10 1 = fac 10 ;;
+
---
+++ typing.ml
@@ -0,0 +1,29 @@
+
+open Lang
+  
+(* Environments *)
+
+type environment = 
+    {localvar: (vname * tp) list; 
+     funbind: (vname * fpdecl) list} ;;
+
+let tp_const = function
+    BoolV _ -> BoolT
+    | IntV _ -> IntT ;;
+    
+let tp_var env var = IntT
+
+(*let function_type_correct tf argsT = match tf with
+                            FunT (a,b) (ta :: reste) -> a = ta*)
+    
+(* Typage d’expressions simples *)
+let rec tp_expr env = function
+    Const c -> tp_const c
+    | VarE v -> tp_var env v
+    | BinOp (binop, e1, e2) -> failwith "a completer"
+    | IfThenElse (cond, e1, e2) -> if tp_expr env cond = BoolT && tp_expr env e1 = tp_expr env e2 then tp_expr env e1 else failwith "condition mal formée"
+    | CallE es -> failwith "a completer" (* filtrage pour déterminer le type d'une fonction *)(*(let ts = List.map (tp_expr env) es in function_type_correct (List.hd ts) (List.lf ts) IfThenElse cond e1 e2)*)
+    | _ -> failwith "a completer" ;;
+
+(* TODO: implement *)
+let tp_prog (Prog (fdfs, e)) = IntT
