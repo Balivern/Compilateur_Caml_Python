fa37b7f356c83ab6dd4851c13976cd002ae53141
Balivern <123549102+Balivern@users.noreply.github.com>
2024-03-25 16:12:19 +0100

    transf_expr

--- transf.ml
+++ transf.ml
@@ -27,3 +27,13 @@ let rec is_tailrec_expr func_name expr = match expr with
                             | BinOp (_, e1, e2) -> is_tailrec_expr func_name e1 && is_tailrec_expr func_name e2
                             | Const exp -> not (exp = func_name)
                             | _ -> true ;;
+
+(* Fonction pour transformer une expression récursive terminale *)
+let rec transf_expr func_name params expr =   if is_tailrec_expr func_name expr 
+                                              then match expr with
+                                                    | IfThenElse (cond, thenBranch, elseBranch) -> Cond(cond, transf_expr func_name params thenBranch, transf_expr func_name params elseBranch)
+                                                    | CallE (f :: args) when f = VarE func_name ->  let assignments = List.map2 (fun param arg -> (param, arg)) params args
+                                                                                                    in Assign(assignments, Skip)
+                                                    | _ -> Skip
+                                              else expr
+                            
